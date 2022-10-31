f8d591197fa2fd0f57b3b85ee59f71dcddf29ae8
Luke Strgar <lukevs@hey.com>
2022-10-31 00:40:40 -0500

    couple updates

--- utils/make_timit.py
+++ utils/make_timit.py
@@ -10,27 +10,31 @@ def main(inpath, outpath):
         return -1 
     if not os.path.exists(outpath):
         os.makedirs(outpath, exist_ok=True)
+        os.makedirs(os.path.join(outpath, 'train'), exist_ok=True)
+        os.makedirs(os.path.join(outpath, 'test'), exist_ok=True)
 
     for _f in tqdm(os.listdir(inpath)):
         parent_f = os.path.join(inpath, _f)
         if os.path.isdir(parent_f):
             for _ff in os.listdir(parent_f):
-                print(_ff)
                 parent_ff = os.path.join(parent_f, _ff)
                 if os.path.isdir(parent_ff):
                     for ex in os.listdir(parent_ff):
-                        if ex.endswith('.PHN'):
-                            src_name = os.path.join(parent_ff, ex)
-                            tgt_name = os.path.join(outpath, _f+'_'+_ff+'_'+ex)
-                            tgt_name = os.path.splitext(tgt_name)[0]+'.phn'
-                            shutil.copy(src_name, tgt_name)
-                        if ex.endswith('.WAV.wav'): 
-                            src_name = os.path.join(parent_ff, ex)
-                            tgt_name = os.path.join(outpath, _f+'_'+_ff+'_'+ex)
-                            tgt_name = os.path.splitext(os.path.splitext(tgt_name)[0])[0]+'.wav'
-                            shutil.copy(src_name, tgt_name)
+                        seg_dir = os.path.join(parent_ff, ex)
+                        if os.path.isdir(seg_dir):
+                            for _file in os.listdir(seg_dir):
+                                if _file.endswith('.PHN'):
+                                    src_name = os.path.join(seg_dir, _file)
+                                    tgt_name = os.path.join(outpath, _f.lower(), _ff+'_'+_file)
+                                    tgt_name = os.path.splitext(tgt_name)[0]+'.phn'
+                                    shutil.copy(src_name, tgt_name)
+                                if _file.endswith('.WAV.wav'): 
+                                    src_name = os.path.join(seg_dir, _file)
+                                    tgt_name = os.path.join(outpath, _f.lower(), _ff+'_'+_file)
+                                    tgt_name = os.path.splitext(os.path.splitext(tgt_name)[0])[0]+'.wav'
+                                    shutil.copy(src_name, tgt_name)
 
-parser = argparse.ArgumentParser(description='Make TIMIT dataset ready for unsupervised segmentation.')
+parser = argparse.ArgumentParser(description='Make TIMIT dataset ready phone segmentation')
 parser.add_argument('--inpath', type=str, required=True, help='the path to the base timit dir.')
 parser.add_argument('--outpath', type=str, required=True, help='the path to save the new format of the data.')
 
